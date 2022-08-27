ce8c27f65cc734731cb01debb12288a05ab4ede8
rivervil16 <agustinriverovila@gmail.com>
2022-08-27 01:44:18 +0000

    Ejercicio Card Profile Generator

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,25 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name || "Gonza"} ${
+    variables.lastname != null ? variables.lastname : "Estevez"
+  }</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
+          
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -56,7 +67,8 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://1.bp.blogspot.com/-IUmC9bFERYg/W2IL1FK-cBI/AAAAAAADPbU/EaJeqNToIcYo8-pHzhGjAgHyC6elJdWKQCLcBGAs/s1600/young-tom-hanks-9.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
